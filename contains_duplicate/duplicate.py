10f2a73382621b70c8e81bea6a5c266b7ee8ce6e
Mkobimbo <122867985+Mkobimbo@users.noreply.github.com>
2024-03-11 22:40:45 +0300

    solution for contains_duplicate

---
+++ contains_duplicate/duplicate.py
@@ -0,0 +1,11 @@
+def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False    
